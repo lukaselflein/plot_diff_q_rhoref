fd49d918ba8bb42a5bb6926c9f3b28284ce3c3e1
Lukas Elflein <elfleinl@cs.uni-freiburg.de>
2019-06-25 15:29:48 +0200

    adapted for new format, add comparison feature

--- plot_difference.py
+++ plot_difference.py
@@ -27,26 +27,24 @@ def stripplot(df):
    df['variable'] = df['variable'].astype(int)
    bp = sns.swarmplot(x='value', y='atom', data=df, hue='variable', 
                       palette=sns.color_palette("coolwarm", len(df.variable.unique())))
-   bp.figure.savefig('stripplot.png')
+   bp.figure.savefig('img/stripplot.png')
 
    
 @default_style
-def cumulative_plot(df):
-   df = df.mean().apply(np.sqrt)
-   df = df.sort_index(ascending=False)
+def cumulative_plot(df, plotname='cumulative.png'):
    p = df.plot(marker='o')
    plt.xlabel('lnrhoref')
    plt.ylabel('sqrt{(q_con - q_uncon)^2 /N} [e]')
-   p.figure.savefig('cumulative.png')
+   p.figure.savefig('img/' + plotname)
 
+def parse_data(constr_file, unconstr_file):
+   print('Reading files {}, {}'.format(constr_file, unconstr_file))
+   df_con = pd.read_csv('data/' + constr_file, decimal='.')
+   df_uncon = pd.read_csv('data/' + unconstr_file, decimal='.')
 
-@default_style
-def main():
-   print('Reading Data ...')
-   df_con = pd.read_csv('constrained_vs_rhoref.csv', decimal='.')
-   df_uncon = pd.read_csv('unconstrained_vs_rhoref.csv', decimal='.')
    dfs = [df_con, df_uncon]
    for df in dfs:
+      df.columns = df.columns.str.replace('q_unconstrained_', '-')
       df = df.set_index(['residue', 'atom'])
       df = df.astype(float)
 
@@ -56,11 +54,52 @@ def main():
    for i in range(-3, -10, -1):
       #diff_df[str(i)] = (df_con[str(i)] - df_uncon[str(i)]).abs()
       diff_df[str(i)] = (df_con[str(i)] - df_uncon[str(i)])**2
-   df = diff_df
+   return diff_df
 
-   print('Plotting ...')
-   stripplot(df)
-   cumulative_plot(df)
+
+def calc_diff(df):
+      df = df.mean().apply(np.sqrt)
+      df = df.sort_index(ascending=False)
+      return df
+
+@default_style
+def compare_plot(df, plotname='cumulative.png'):
+   print('Plotting {}'.format(plotname))
+   p = sns.pointplot(x='lnrho', y='diff', hue='q', data=df)
+   ax = p.axes
+   ax.set_ylim(df['diff'].min() - 0.05, 0.4)
+   #plt.xlabel('lnrhoref')
+   plt.ylabel('sqrt{(q_con - q_uncon)^2 /N} [e]')
+   p.figure.savefig('img/' + plotname)
+
+
+@default_style
+def main():
+   # Zero Charge
+   df_0 = parse_data('constrained_vs_rhoref_q0.csv', 'unconstrained_vs_rhoref_q0.csv')
+   diff_0 = calc_diff(df_0) 
+   cumulative_plot(diff_0, 'cumulative_q0.png')
+
+   # Charge = 1
+   df_1 = parse_data('constrained_vs_rhoref.csv', 'unconstrained_vs_rhoref.csv')
+   diff_1 = calc_diff(df_1) 
+   cumulative_plot(diff_1, 'cumulative_q1.png')
+
+   # Charge = 2
+   df_2 = parse_data('constrained_vs_rhoref_q2.csv', 'unconstrained_vs_rhoref_q2.csv')
+   diff_2 = calc_diff(df_2) 
+   cumulative_plot(diff_2, 'cumulative_q2.png')
+
+   # Combine everything into one dataframe
+   df = pd.DataFrame(index=diff_1.index)
+   df['0'] = diff_0.values
+   df['1'] = diff_1.values
+   df['2'] = diff_2.values
+   df['lnrho'] = df.index
+   df = df.melt(id_vars=['lnrho'], var_name='q', value_name='diff')
+   df['q'] = df['q'].astype(int)
+   compare_plot(df, plotname='lnrho_q_comparison.png')
+
    print('Done.')
    
 
